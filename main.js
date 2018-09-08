420b05367f58615522c6c1fe205e350d643b27db
zbo14 <zbalder@starry.com>
2018-09-08 00:54:24 -0400

    initial

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+const createWindow = () => {
+  win = new BrowserWindow({ width: 800, height: 600 })
+  win.loadFile('index.html')
+
+  if (process.env.NODE_ENV === 'development') {
+    win.webContents.openDevTools()
+  }
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
